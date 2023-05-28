52372bca3e9dce087c61e818fce605fbfc1b9c47
temmie_950807 <kapoo950807@gmail.com>
2023-05-28 22:24:35 +0800

    5/28 update

--- 比賽/中文賽區/板中比賽/A_大電繩的煩惱_Rope.cpp
+++ 比賽/中文賽區/板中比賽/A_大電繩的煩惱_Rope.cpp
@@ -3,30 +3,31 @@
 #include <ext/pb_ds/tree_policy.hpp>
 #pragma GCC optimize("O3,unroll-loops")
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define dbg(x) cerr << #x << " = " << x << endl
 #define int long long
+#if !LOCAL
+#define endl "\n"
+#endif
 using namespace std;
 using namespace __gnu_pbds;
 typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> order_set;
 struct custom_hash {
-  static uint64_t splitmix64(uint64_t x) {
-    // http://xorshift.di.unimi.it/splitmix64.c
-    x += 0x9e3779b97f4a7c15;
-    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-    return x ^ (x >> 31);
-  }
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
 
-  size_t operator()(uint64_t x) const {
-    static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-    return splitmix64(x + FIXED_RANDOM);
-  }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
 };
 
 // debugger
 // ===================================
-bool debug_mode=true;
-#define cerr if(debug_mode) cerr
-#define dbg(x) cerr << #x << " = " << x << endl
 template<typename T>void debug(const T &v,int h,int w,string sv=" "){for(int i=0;i<h;i++){cerr<<v[i][0];for(int j=1;j<w;j++)cerr<<sv<<v[i][j];cerr<<endl;}};
 template<typename T>void debug(const T &v,int n,string sv=" "){if(n!=0)cerr<<v[0];for(int i=1;i<n;i++)cerr<<sv<<v[i];cerr<<endl;};
 template<typename T>void debug(const vector<T>&v){debug(v,v.size());}
@@ -43,26 +44,41 @@ template<typename T,size_t size>void debug(const array<T, size> &a){for(auto z:a
 // ===================================
 
 // declare
-const int MAX_N = 2e5+5;
+const int MAX_SIZE = 2e5+5;
 const int INF = 9e18;
 const int MOD = 1e9+7;
 const double EPS = 1e-6;
 
-int n, tmp;
+int n, k, total, tmp;
 vector<int> v;
 
 void solve1(){
-  
-  return;
+    
+    // input
+    cin >> n >> k;
+    for (int i=0 ; i<n ; i++){
+        cin >> tmp;
+        total+=tmp;
+        v.push_back(tmp);
+    }
+
+    if (total==k){
+        cout << n-1 << endl;
+    }else if (total<k){
+        cout << n << endl;
+    }else{
+        cout << -1 << endl;
+    }
+
+    return;
 }
 
 signed main(void){
-  fastio;
-  
-  int t=1;
-  cin >> t;
-  while (t--){
-    solve1();
-  }
-  return 0;
+    fastio;
+    
+    int t=1;
+    while (t--){
+        solve1();
+    }
+    return 0;
 }
